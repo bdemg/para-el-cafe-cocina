58c815063d511b992a6e367e7d94a0577ace0578
Jorge Cano <jacano.sosa@gmail.com>
2016-10-05 11:45:07 -0500

    Clase Ingredients ya tiene ingredientes

--- src/model/recipe/Ingredients.java
+++ src/model/recipe/Ingredients.java
@@ -2,7 +2,32 @@ package model.recipe;
 
 public class Ingredients {
     
-    static String EGGS = "Huevos";
-    static String FLOUR = "Harina";
+    static final String EGGS = "Huevos";
+    static final String FLOUR = "Harina";
+    static final String BUTTER = "Mantequilla";
+    static final String SUGAR = "Azucar";
+    static final String BAKING_POWDER = "Polvo para hornear";
+    static final String ORANGE_JUICE = "Jugo de naranja";
+    static final String SCRATCHES = "Rayadura";
+    static final String LEMON_JUICE = "Jugo de limón";
+    static final String WATER = "Agua";
+    static final String STABILIZER = "Estabilizador";
+    static final String YEAST = "Levadura";
+    static final String MARGARINE = "Margarina";
+    static final String VEGETAL_OIL = "Aceite vegetal";
+    static final String CRUSHED_NUTS = "Nuez molida";
+    static final String GLASS_SUGAR = "Azucar Glass";
+    static final String CONDENSED_MILK = "Leche Condensada";
+    static final String CHOCOLATE = "Chocolate";
+    static final String VANILLA = "Vainilla";
+    static final String SALT = "Sal";
+    static final String NUTS = "Nuez";
+    static final String CREAM_CHEESE = "Queso Crema";
+    static final String MILK = "Leche";
+    static final String MARIA_COOKIES = "Galleta María";
+    static final String VEGETAL_LARD = "Manteca vegetal";
+    static final String CHEDDAR_CHEESE = "Queso Cheddar";
+    static final String MONTERREY_JACK_CHEESE = "Queso Monterrey Jack";
+    static final String PEPPER = "Pimienta";
     
 }
