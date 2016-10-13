f15a166843f14df5ef6aa30556ecab4623a5f8da
Ivan Castellanos <eivan0castellanos@gmail.com>
2016-10-12 19:04:17 -0500

    clase Cookbook actualizada para contener más recetas

--- src/model/Cookbook.java
+++ src/model/Cookbook.java
@@ -1,15 +1,27 @@
 package model;
 
+import model.recipe.BrownieRecipe;
+import model.recipe.CinnamonRollRecipe;
+import model.recipe.ItalianMeringueRecipe;
 import model.recipe.OrangeCakeRecipe;
+import model.recipe.PaulaCheeseBisquetRecipe;
+import model.recipe.PerfectCupCakeRecipe;
 import model.recipe.Recipe;
+import model.recipe.StrawberryCakeRecipe;
 
 public class Cookbook {
     
     private static final Cookbook instance = new Cookbook();
     public static final int INGREDIENT_QUANTITY = 1;
     
-    private final String ORANGECAKE = "Pastel de Naranja";
-    
+    private final String ORANGE_CAKE = "Pastel de Naranja";
+    private final String BROWNIE = "Brownies";
+    private final String CINNAMONROLL = "Roles de Canela";
+    private final String ITALIAN_MERINGUE = "Merengue Italiano";
+    private final String PAULA_CHEESE_BISQUET = "Bisquet de Queso Paula";
+    private final String PERFECT_CUPCAKE = "Cupcake perfecto";
+    private final String STRABERRY_CAKE = "Pastel de Fresas";
+            
     private Cookbook(){}
     
     public static Cookbook getInstance(){
@@ -19,8 +31,32 @@ public class Cookbook {
     public Recipe getRecipe(String inputProductName){
         Recipe requestedRecipe =  null;
         switch(inputProductName){
-            case ORANGECAKE:
+            case ORANGE_CAKE:
                 requestedRecipe = new OrangeCakeRecipe();
+                break;
+                
+            case BROWNIE:
+                requestedRecipe = new BrownieRecipe();
+                break;
+                
+            case CINNAMONROLL:
+                requestedRecipe = new CinnamonRollRecipe();
+                break;
+                
+            case ITALIAN_MERINGUE:
+                requestedRecipe = new ItalianMeringueRecipe();
+                break;
+                
+            case PAULA_CHEESE_BISQUET:
+                requestedRecipe = new PaulaCheeseBisquetRecipe();
+                break;
+                
+            case PERFECT_CUPCAKE:
+                requestedRecipe = new PerfectCupCakeRecipe();
+                break;
+                
+            case STRABERRY_CAKE:
+                requestedRecipe = new StrawberryCakeRecipe();
                 break;
         }
         return requestedRecipe;
